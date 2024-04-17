35f39e513055e43399d310e1128c6116156b30a2
Tom Winter <tom@aam-digital.com>
2024-04-17 20:33:24 +0200

    feat: add HelloFriendController for root route

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/Application.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/Application.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,4 +14,13 @@ class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
+
+@RestController
+class HelloFriendController {
+    @GetMapping("/")
+    @Suppress("FunctionOnlyReturningConstant")
+    fun helloFriend(): String {
+        return "Hello, Friend. This is aam-backend-service."
+    }
 }
