19ef74c0a88699ca1d1db13bbd84701e9dfa61b8
Tom Winter <tom@aam-digital.com>
2024-07-23 13:09:37 +0200

    Update application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/reportcalculation/controller/ReportCalculationController.kt
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/reportcalculation/controller/ReportCalculationController.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/reportcalculation/controller/ReportCalculationController.kt
@@ -102,7 +102,7 @@ class ReportCalculationController(
     fun fetchReportCalculationData(
         @PathVariable calculationId: String
     ): Flux<DataBuffer> {
-        // TODO Auth check (?)
+        // TODO Auth check (https://github.com/Aam-Digital/aam-services/issues/10)
 
         return reportingStorage.headData(DomainReference(calculationId))
             .toFlux()
