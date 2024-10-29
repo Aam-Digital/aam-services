1a1fe8c08943fee013213b80ce26673c8effe785
Tom Winter <tom@aam-digital.com>
2024-10-29 19:00:54 +0100

    docs: add code comment

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/events/SequenceGenerator.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/events/SequenceGenerator.kt
@@ -3,6 +3,8 @@ package com.aamdigital.aambackendservice.events
 import java.util.concurrent.atomic.AtomicLong
 
 // TODO -> needs database implementation or file persistence
+// is needed to get system wide unique identifier for events
+// easiest way is to use an Sequence from e.g. Postgres
 object SequenceGenerator {
     private val currentId = AtomicLong(0)
 
@@ -10,4 +12,3 @@ object SequenceGenerator {
         return currentId.incrementAndGet()
     }
 }
-
