7d21b6caa5093fd6d75cda13da7faa3b86987af6
Tom Winter <tom@aam-digital.com>
2024-11-15 18:06:41 +0100

    fix: correctly set the request body in webhooks (#51)

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/notification/core/DefaultTriggerWebhookUseCase.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/notification/core/DefaultTriggerWebhookUseCase.kt
@@ -2,6 +2,7 @@ package com.aamdigital.aambackendservice.reporting.notification.core
 
 import com.aamdigital.aambackendservice.domain.DomainReference
 import com.aamdigital.aambackendservice.reporting.domain.event.NotificationEvent
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
@@ -16,6 +17,7 @@ class DefaultTriggerWebhookUseCase(
     private val notificationStorage: NotificationStorage,
     private val httpClient: RestClient,
     private val uriParser: UriParser,
+    private val objectMapper: ObjectMapper,
 ) : TriggerWebhookUseCase {
     private val logger = LoggerFactory.getLogger(javaClass)
 
@@ -44,9 +46,12 @@ class DefaultTriggerWebhookUseCase(
             .headers {
                 it.set(HttpHeaders.AUTHORIZATION, "Token ${webhook.authentication.secret}")
             }
+            .contentType(MediaType.APPLICATION_JSON)
             .body(
-                mapOf(
-                    Pair("calculation_id", notificationEvent.calculationId)
+                objectMapper.writeValueAsString(
+                    hashMapOf(
+                        "calculation_id" to notificationEvent.calculationId
+                    )
                 )
             )
             .accept(MediaType.APPLICATION_JSON)
