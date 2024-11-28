a1a6e5f96fca0bfac0e97ac9e9119648bfe345d1
Tom Winter <tom@aam-digital.com>
2024-11-28 21:05:26 +0100

    feat: at user-profile/id endpoint

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/skill/controller/SkillController.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/skill/controller/SkillController.kt
@@ -6,9 +6,15 @@ import com.aamdigital.aambackendservice.skill.core.FetchUserProfileUpdatesReques
 import com.aamdigital.aambackendservice.skill.core.FetchUserProfileUpdatesUseCase
 import com.aamdigital.aambackendservice.skill.core.SearchUserProfileRequest
 import com.aamdigital.aambackendservice.skill.core.SearchUserProfileUseCase
+import com.aamdigital.aambackendservice.skill.domain.EscoSkill
+import com.aamdigital.aambackendservice.skill.domain.SkillUsage
+import com.aamdigital.aambackendservice.skill.domain.UserProfile
+import com.aamdigital.aambackendservice.skill.repository.SkillLabUserProfileRepository
 import com.aamdigital.aambackendservice.skill.skilllab.SkillLabFetchUserProfileUpdatesErrorCode
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController
 class SkillController(
     private val fetchUserProfileUpdatesUseCase: FetchUserProfileUpdatesUseCase, // todo needs no-op implementation
     private val searchUserProfileUseCase: SearchUserProfileUseCase, // todo needs no-op implementation
+    private val userProfileRepository: SkillLabUserProfileRepository
 ) {
 
     @GetMapping("/user-profile")
@@ -61,6 +68,39 @@ class SkillController(
             }
 
             is UseCaseOutcome.Success<*> -> ResponseEntity.ok().body(result.data)
+        }
+    }
+
+    @GetMapping("/user-profile/{id}")
+    fun fetchUserProfile(
+        @PathVariable id: String,
+    ): ResponseEntity<Any> {
+        return if (!userProfileRepository.existsByExternalIdentifier(id)) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                HttpErrorDto(
+                    errorCode = "NOT_FOUND",
+                    errorMessage = "No UserProfile found with this id: $id"
+                )
+            )
+        } else {
+            val entity = userProfileRepository.findByExternalIdentifier(id)
+            ResponseEntity.ok().body(
+                UserProfile(
+                    id = entity.externalIdentifier,
+                    fullName = entity.fullName,
+                    phone = entity.mobileNumber,
+                    email = entity.email,
+                    skills = entity.skills.map {
+                        EscoSkill(
+                            usage = SkillUsage.valueOf(it.usage.uppercase()),
+                            escoUri = it.externalIdentifier
+                        )
+                    },
+                    updatedAtExternalSystem = entity.updatedAt,
+                    importedAt = entity.importedAt?.toInstant(),
+                    latestSyncAt = entity.latestSyncAt?.toInstant(),
+                )
+            )
         }
     }
 
