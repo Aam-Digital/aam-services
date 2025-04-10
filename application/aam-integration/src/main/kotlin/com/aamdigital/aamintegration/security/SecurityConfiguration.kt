b0fa172628d3088e9836a7a04b28e967de3fb9db
Tom Winter <tom@aam-digital.com>
2025-04-10 11:11:15 +0200

    feat: multi issuer support

--- application/aam-integration/src/main/kotlin/com/aamdigital/aamintegration/security/SecurityConfiguration.kt
+++ application/aam-integration/src/main/kotlin/com/aamdigital/aamintegration/security/SecurityConfiguration.kt
@@ -1,19 +1,33 @@
 package com.aamdigital.aamintegration.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.nimbusds.jwt.SignedJWT
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration {
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private lateinit var issuerUri: String
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri-application:}")
+    private lateinit var issuerUriApplication: String
 
     @Bean
     fun filterChain(
@@ -52,16 +66,37 @@ class SecurityConfiguration {
                     )
             }
             oauth2ResourceServer {
-                jwt {
-                    jwtAuthenticationConverter = aamAuthenticationConverter
-                    authenticationEntryPoint =
-                        AamAuthenticationEntryPoint(
-                            parentEntryPoint = BearerTokenAuthenticationEntryPoint(),
-                            objectMapper = objectMapper
-                        )
-                }
+                authenticationManagerResolver = authenticationManagerResolver(aamAuthenticationConverter)
+                authenticationEntryPoint =
+                    AamAuthenticationEntryPoint(
+                        parentEntryPoint = BearerTokenAuthenticationEntryPoint(),
+                        objectMapper = objectMapper
+                    )
             }
         }
         return http.build()
+    }
+
+    @Bean
+    fun authenticationManagerResolver(
+        aamAuthenticationConverter: AamAuthenticationConverter
+    ): JwtIssuerAuthenticationManagerResolver {
+        return JwtIssuerAuthenticationManagerResolver { token ->
+            val issuer = try {
+                SignedJWT.parse(token).jwtClaimsSet.issuer
+            } catch (e: Exception) {
+                throw BadCredentialsException("Could not parse issuer.", e)
+            }
+
+            if (listOf(issuerUri, issuerUriApplication).contains(issuer).not()) {
+                throw BadCredentialsException("Untrusted issuer: $issuer")
+            }
+
+            val decoder: JwtDecoder = JwtDecoders.fromIssuerLocation(issuer)
+            val provider = JwtAuthenticationProvider(decoder)
+            provider.setJwtAuthenticationConverter(aamAuthenticationConverter)
+
+            ProviderManager(provider)
+        }
     }
 }
