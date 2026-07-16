42cf6ef000bae180187eded75354d8656e878bb4
Sebastian Leidig <sebastian@aam-digital.com>
2026-07-16 14:29:45 +0200

    dlx

--- application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/reportcalculation/di/ReportCalculationQueueConfiguration.kt
+++ application/aam-backend-service/src/main/kotlin/com/aamdigital/aambackendservice/reporting/reportcalculation/di/ReportCalculationQueueConfiguration.kt
@@ -8,8 +8,12 @@ import com.aamdigital.aambackendservice.reporting.reportcalculation.queue.Report
 import com.aamdigital.aambackendservice.reporting.reportcalculation.usecase.DefaultReportCalculationUseCase
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.micrometer.observation.ObservationRegistry
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.FanoutExchange
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.QueueBuilder
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,6 +23,8 @@ class ReportCalculationQueueConfiguration {
     companion object {
         const val REPORT_CALCULATION_EVENT_QUEUE = "report.calculation"
         const val REPORT_CALCULATION_COMPLETED_QUEUE = "report.calculation.completed"
+        const val REPORT_CALCULATION_COMPLETED_DEAD_LETTER_QUEUE = "$REPORT_CALCULATION_COMPLETED_QUEUE.deadLetter"
+        const val REPORT_CALCULATION_COMPLETED_DEAD_LETTER_EXCHANGE = "$REPORT_CALCULATION_COMPLETED_QUEUE.dlx"
     }
 
     @Bean("report-calculation-event-queue")
@@ -31,8 +37,24 @@ class ReportCalculationQueueConfiguration {
     fun reportCalculationCompletedQueue(): Queue =
         QueueBuilder
             .durable(REPORT_CALCULATION_COMPLETED_QUEUE)
+            .deadLetterExchange(REPORT_CALCULATION_COMPLETED_DEAD_LETTER_EXCHANGE)
             .build()
 
+    @Bean("report-calculation-completed-dead-letter-queue")
+    fun reportCalculationCompletedDeadLetterQueue(): Queue =
+        QueueBuilder
+            .durable(REPORT_CALCULATION_COMPLETED_DEAD_LETTER_QUEUE)
+            .build()
+
+    @Bean("report-calculation-completed-dead-letter-exchange")
+    fun reportCalculationCompletedDeadLetterExchange(): FanoutExchange =
+        FanoutExchange(REPORT_CALCULATION_COMPLETED_DEAD_LETTER_EXCHANGE)
+
+    @Bean
+    fun reportCalculationCompletedDeadLetterBinding(
+        @Qualifier("report-calculation-completed-dead-letter-queue") reportCalculationCompletedDeadLetterQueue: Queue,
+        @Qualifier("report-calculation-completed-dead-letter-exchange") reportCalculationCompletedDeadLetterExchange: FanoutExchange
+    ): Binding = BindingBuilder.bind(reportCalculationCompletedDeadLetterQueue).to(reportCalculationCompletedDeadLetterExchange)
     @Bean
     fun reportCalculationEventListener(
         observationRegistry: ObservationRegistry,
@@ -54,3 +76,4 @@ class ReportCalculationQueueConfiguration {
     ): ReportCalculationCompletedEventConsumer =
         ReportCalculationCompletedEventConsumer(observationRegistry, reportCalculationChangeUseCase)
 }
+
